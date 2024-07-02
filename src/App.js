e42d1a8c6c6498f7026576bc825bef8f729f48af
Asbel Torales <toralesasbel@gmail.com>
2024-07-01 19:15:25 -0600

    comments added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// Importación de App.css y bibliotecas necesarias
 import './App.css';
 import Axios from 'axios';
 import { useEffect, useState, useReducer } from 'react';
@@ -15,16 +16,17 @@ import { format } from 'date-fns';
 import { Box } from '@mui/material';
 
 
-
+// Estado inicial 
 const initialState = {
   posts: JSON.parse(localStorage.getItem('posts')) || [],
 };
 
+// Función reducer para manejar el estado
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'SET_POSTS':
+    case 'SET_POSTS':  // Establece las publicaciones
       return { ...state, posts: action.payload };
-    case 'TOGGLE_CHECKBOX':
+    case 'TOGGLE_CHECKBOX': // Alterna el estado de la casilla de verificación
       return {
         ...state,
         posts: state.posts.map((post, index) =>
@@ -34,7 +36,7 @@ const reducer = (state, action) => {
         ),
     };
 
-    case 'MARK_ALL_AS_CHECKED':
+    case 'MARK_ALL_AS_CHECKED': // Marca todas las publicaciones como leídas
       return {
         ...state,
         posts: state.posts.map(post => ({ ...post, checked: true })),
@@ -44,12 +46,14 @@ const reducer = (state, action) => {
   }
 };
 
+// Componente principal de la aplicación
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  //const [posts, setPosts] = useState([]);
+  const [state, dispatch] = useReducer(reducer, initialState); // Inicializa el estado y el despachador
+  //const [posts, setPosts] = useState([]); 
 
   console.log(state)
 
+// Llamada a la API de WordPress para obtener las publicaciones al montar el componente
   useEffect(() => {
     Axios.get('https://fernandafamiliar.soy/wp-json/wp/v2/posts/').then((res) => {
       const postsWithReadStatus = res.data.map(post => ({ ...post, checked: false }));
@@ -58,32 +62,34 @@ function App() {
     });
   }, []);
 
+// Guarda el estado actual de las publicaciones en el almacenamiento local cada vez que cambia
   useEffect(() => {
-    //Almacena las notas en el almacenamiento local del navegador 
     localStorage.setItem('posts', JSON.stringify(state.posts)); 
   }, [state.posts]);
 
+// Función para alternar el estado de la casilla de verificación de una publicación específica
   const toggleCheckbox = (index) => {
     dispatch({ type: 'TOGGLE_CHECKBOX', payload: index });
   };
 
+// Función para marcar todas las publicaciones como leídas
   const markAllAsChecked = () => {
     dispatch({ type: 'MARK_ALL_AS_CHECKED' });
   };
 
   return (
-  <div style={{ padding: '20px' }}>
-    <Box mb={2} display="flex" justifyContent="center">
-      <Button variant="contained" color="primary" onClick={markAllAsChecked}>
+<div style={{ padding: '20px' }}>
+  <Box mb={2} display="flex" justifyContent="center">
+    <Button variant="contained" color="primary" onClick={markAllAsChecked}>
         Marcar todas como leidas
-      </Button>
-    </Box>
-    <Container fixed>
+    </Button>
+  </Box>
+  <Container fixed>
     <Grid container spacing={3} justifyContent="center">
       {state.posts.map((post, index) => (
       <Grid item xs={12} key={index} sm={6} md={4}>
         <Card 
-        sx={{ maxWidth: 345, boxShadow: 3, ':hover':{ boxShadow: 20,} }}>
+         sx={{ maxWidth: 345, boxShadow: 3, ':hover':{ boxShadow: 20,} }}>
           <CardMedia
              sx={{ height: 200 }}
              src= {post.jetpack_featured_media_url}
@@ -110,14 +116,14 @@ function App() {
 
                   <div style={{ marginTop: '1.5rem' }}>
                     <Typography
-                    component="p"
-                    variant="caption"
-                    color="textSecondary"
-                    gutterBottom
+                      component="p"
+                      variant="caption"
+                      color="textSecondary"
+                      gutterBottom
                   
-                  >
+                    >
                     Publicación: {format(new Date(post.date), 'dd/MM/yyyy')} - Hora: {format(new Date(post.date), 'HH:mm:ss')}
-                  </Typography>
+                    </Typography>
 
 
                     <Typography
@@ -147,8 +153,8 @@ function App() {
       </Grid>
      ))}
     </Grid>
-    </Container>
-  </div>
+  </Container>
+</div>
   );
 }
 export default App;
